03ee95226da7c9e309be6df9b14d88e1921aa8b8
andresmestra <andresmestra11a@gmail.com>
2023-04-26 20:19:03 -0500

    feat: add cut paper in example and add pico css

--- example/src/js/capacitor-welcome.js
+++ example/src/js/capacitor-welcome.js
@@ -13,15 +13,23 @@ async function handleSubmit() {
   const qrString = document.getElementById('qrCodeElement').value;
   const asciiFontType = document.getElementById('asciiFontType').value;
   const cFontType = document.getElementById('cFontType').value;
+  const cutModeString = document.getElementById('cutMode').value;
+  const cutMode = Number.parseInt(cutModeString);
+
+  let payload = {
+    text,
+    asciiFontType,
+    cFontType,
+  };
+
+  if (qrString?.length > 0) {
+    payload.qrString = qrString;
+  }
 
   try {
-    const resp = await PaxPrinterUtilityPlugin.printReceiptWithQr({
-      text,
-      qrString,
-      asciiFontType,
-      cFontType,
-    });
-    console.log({ resp });
+    const resp = await PaxPrinterUtilityPlugin.printReceiptWithQr(payload);
+    const cutPaper = await PaxPrinterUtilityPlugin.cutPaper({ mode: cutMode });
+    console.log({ resp, cutPaper });
   } catch (error) {
     console.error(error);
   }
